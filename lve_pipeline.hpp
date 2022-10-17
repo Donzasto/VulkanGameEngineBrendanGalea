1e049683ea2aead69cc429df90646e791a6f4464
Donzasto <fordealing@yandex.ru>
2022-10-17 10:21:39 +0300

    Swap Chain Recreation & Dynamic Viewports - Vulkan Game Engine Tutorial 08

--- lve_pipeline.hpp
+++ lve_pipeline.hpp
@@ -6,44 +6,51 @@
 #include <string>
 #include <vector>
 
-namespace lve {
+namespace lve
+{
 
-  struct PipelineConfigInfo {
-  VkViewport viewport;
-  VkRect2D scissor;
-  VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
-  VkPipelineRasterizationStateCreateInfo rasterizationInfo;
-  VkPipelineMultisampleStateCreateInfo multisampleInfo;
-  VkPipelineColorBlendAttachmentState colorBlendAttachment;
-  VkPipelineColorBlendStateCreateInfo colorBlendInfo;
-  VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
-  VkPipelineLayout pipelineLayout = nullptr;
-  VkRenderPass renderPass = nullptr;
-  uint32_t subpass = 0;
-};
-  class LvePipeline {
+  struct PipelineConfigInfo
+  {
+    PipelineConfigInfo(const PipelineConfigInfo&) = delete;
+    PipelineConfigInfo& operator=(const PipelineConfigInfo&) = delete;
+
+    VkPipelineViewportStateCreateInfo viewportInfo;
+    VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
+    VkPipelineRasterizationStateCreateInfo rasterizationInfo;
+    VkPipelineMultisampleStateCreateInfo multisampleInfo;
+    VkPipelineColorBlendAttachmentState colorBlendAttachment;
+    VkPipelineColorBlendStateCreateInfo colorBlendInfo;
+    VkPipelineDepthStencilStateCreateInfo depthStencilInfo;
+    std::vector<VkDynamicState> dynamicStateEnables;
+    VkPipelineDynamicStateCreateInfo dynamicStateInfo;
+    VkPipelineLayout pipelineLayout = nullptr;
+    VkRenderPass renderPass = nullptr;
+    uint32_t subpass = 0;
+  };
+  class LvePipeline
+  {
   public:
-    LvePipeline(LveDevice &device, const std::string& vertFilepath, const std::string& fragFilepath, const PipelineConfigInfo &configInfo);
-   ~LvePipeline();
+    LvePipeline(LveDevice &device, const std::string &vertFilepath, const std::string &fragFilepath, const PipelineConfigInfo &configInfo);
+    ~LvePipeline();
 
-   LvePipeline(const LvePipeline&) = delete;
-   LvePipeline& operator=(const LvePipeline&) = delete;
+    LvePipeline(const LvePipeline &) = delete;
+    LvePipeline &operator=(const LvePipeline &) = delete;
 
-   void bind(VkCommandBuffer commandBuffer);
+    void bind(VkCommandBuffer commandBuffer);
 
-   static PipelineConfigInfo defaultPipelineConfigInfo(uint32_t width, uint32_t height);
+    static void defaultPipelineConfigInfo(PipelineConfigInfo& configInfo);
 
   private:
-    static std::vector<char> readFile(const std::string& filepath);
+    static std::vector<char> readFile(const std::string &filepath);
 
-    void createGraphicsPipeline(const std::string& vertFilepath,
-                               const std::string& fragFilepath,
-                               const PipelineConfigInfo& configInfo);
+    void createGraphicsPipeline(const std::string &vertFilepath,
+                                const std::string &fragFilepath,
+                                const PipelineConfigInfo &configInfo);
 
-    void createShaderModule(const std::vector<char>& code,
-                            VkShaderModule* shaderModule);
+    void createShaderModule(const std::vector<char> &code,
+                            VkShaderModule *shaderModule);
 
-    LveDevice& lveDevice;
+    LveDevice &lveDevice;
     VkPipeline graphicsPipeline;
     VkShaderModule vertShaderModule;
     VkShaderModule fragShaderModule;
--- lve_swap_chain.cpp
+++ lve_swap_chain.cpp
@@ -14,6 +14,18 @@ namespace lve {
 LveSwapChain::LveSwapChain(LveDevice &deviceRef,
                                      VkExtent2D extent)
     : device{deviceRef}, windowExtent{extent} {
+  init();
+}
+
+LveSwapChain::LveSwapChain(LveDevice &deviceRef,
+                                     VkExtent2D extent, std::shared_ptr<LveSwapChain> previos)
+    : device{deviceRef}, windowExtent{extent}, oldSwapChain{previos} {
+  init();
+
+  oldSwapChain = nullptr;
+}
+
+void LveSwapChain::init(){
   createSwapChain();
   createImageViews();
   createRenderPass();
@@ -162,7 +174,7 @@ void LveSwapChain::createSwapChain() {
   createInfo.presentMode = presentMode;
   createInfo.clipped = VK_TRUE;
 
-  createInfo.oldSwapchain = VK_NULL_HANDLE;
+  createInfo.oldSwapchain = oldSwapChain == nullptr ? VK_NULL_HANDLE : oldSwapChain->swapChain;
 
   if (vkCreateSwapchainKHR(device.device(), &createInfo, nullptr, &swapChain) !=
       VK_SUCCESS) {
